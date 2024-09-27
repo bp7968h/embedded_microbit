1782af5e5d4011ecf41617adaf258d7c5a04f684
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-09-27 23:56:39 +0100

    feat: spiral display

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,101 @@
 #![deny(unsafe_code)]
-//dont't use standard crate
 #![no_std]
-//don't use standard main interface
 #![no_main]
 
 use cortex_m_rt::entry;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 use panic_halt as _;
-use microbit::board::Board;
-use microbit::hal::timer::Timer;
-use microbit::hal::prelude::*;
-
 
 #[entry]
 fn entry_point() -> ! {
+    // Initialize the board, timer, and display only once
     let mut board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-    board.display_pins.col1.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    // Initialize the LED matrix
+    let mut leds = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let row_len = leds.len();
+    let col_len = leds[0].len();
 
     loop {
-        row1.set_low().unwrap();
-        timer.delay_ms(1_000_u16);
-        row1.set_high().unwrap();
-        timer.delay_ms(1_000_u16);
+        // Call spiral_display, passing display and timer
+        spiral_display(
+            &mut display,
+            &mut timer,
+            &mut leds,
+            0,
+            row_len - 1,
+            0,
+            col_len - 1,
+        );
     }
+}
+
+fn spiral_display(
+    display: &mut Display, 
+    timer: &mut Timer<microbit::hal::pac::TIMER0>,  // Pass Timer and Display by reference
+    leds: &mut [[u8; 5]; 5],
+    start_row: usize,
+    end_row: usize,
+    start_col: usize,
+    end_col: usize,
+) {
+    if start_row > end_row || start_col > end_col {
+        return;
+    }
+
+    // Top row (left to right)
+    for i in start_col..=end_col {
+        leds[start_row][i] = 1;
+        display.show(timer, *leds, 30);
+        leds[start_row][i] = 0;
+    }
+
+    // Right column (top to bottom)
+    for i in (start_row + 1)..=end_row {
+        leds[i][end_col] = 1;
+        display.show(timer, *leds, 30);
+        leds[i][end_col] = 0;
+    }
+
+    // Bottom row (right to left)
+    if start_row != end_row {
+        for i in (start_col..end_col).rev() {
+            leds[end_row][i] = 1;
+            display.show(timer, *leds, 30);
+            leds[end_row][i] = 0;
+        }
+    }
+
+    // Left column (bottom to top)
+    if start_col != end_col {
+        for i in (start_row + 1..end_row).rev() {
+            leds[i][start_col] = 1;
+            display.show(timer, *leds, 30);
+            leds[i][start_col] = 0;
+        }
+    }
+
+    // Recursively call the next inner layer of the spiral
+    spiral_display(
+        display,
+        timer,
+        leds,
+        start_row + 1,
+        end_row - 1,
+        start_col + 1,
+        end_col - 1,
+    );
 }
